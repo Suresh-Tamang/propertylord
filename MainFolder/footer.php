f4fbc3a56aa25fa46673e01a1691692dedeac747
Suresh-Tamang <sureshjimba3333@gmail.com>
2024-05-20 02:32:54 +0545

    main folder added

---
+++ MainFolder/footer.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/footer.css">
+    <link rel="stylesheet"
+    href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="footer">
+        <div class="footr-content">
+            <img src="img/logo.png" alt="" srcset="">
+            <p></p>
+            <div class="icons">
+                <a:href><i class='bx bxl-facebook-circle'></i></a:href>
+                <a href=""><i class='bx bxl-instagram'></i></a>
+                <a href=""><i class='bx bxl-linkedin'></i></a>
+                <a href=""><i class='bx bxl-twitter'></i></a>
+                <a href=""><i class='bx bxl-youtube'></i></a>
+            </div>
+        </div>
+        <div class="footer-content">
+            <h4>Projects</h4>
+            <li><a href="">Houses</a></li>
+            <li><a href="">Rooms</a></li>
+            <li><a href="">Flats</a></li>
+            <li><a href="">Appartments</a></li>
+        </div>
+        <div class="footer-content">
+            <h4>Company</h4>
+            <li><a href="">How we work</a></li>
+            <li><a href="">Capital</a></li>
+            <li><a href="">Security</a></li>
+            <li><a href="">Sellings</a></li>
+        </div>
+        <div class="footer-content">
+            <h4>Movement</h4>
+            <li><a href="">Movement</a></li>
+            <li><a href="">Pricing</a></li>
+            <li><a href="">Renting</a></li>
+            <li><a href="">Sellings</a></li>
+        </div>
+        <div class="footer-content">
+            <h4>Help</h4>
+            <li><a href="">Privacy</a></li>
+            <li><a href="">Condition</a></li>
+            <li><a href="">Blogs</a></li>
+            <li><a href="">FAQ</a></li>
+        </div>
+    </section>
+</body>
+</html>
