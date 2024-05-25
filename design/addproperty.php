ddf8b41a62c338a8a1f8181c73dd48a633e0f6c6
Suresh-Tamang <sureshjimba3333@gmail.com>
2024-05-25 10:35:33 +0545

    managed

--- design/addproperty.php
+++ design/addproperty.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Propertylord</title>
-    <link rel="stylesheet" href="registration.css">
+    <link rel="stylesheet" href="addproperty.css">
     <!-- boxi icon links -->
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">    
 </head>
