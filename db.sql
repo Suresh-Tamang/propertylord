4025e451782a9cfa94f00ca910d7866176a65599
Suresh-Tamang <sureshjimba3333@gmail.com>
2024-05-25 08:14:01 +0545

    managed

---
+++ db.sql
@@ -0,0 +1,23 @@
+ create table locations(
+    id int PRIMARY KEY,
+    district varchar(20)
+ );
+    create table users(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    firstname varchar(20),
+    lastname varchar(20),
+    email varchar(90),
+    contact varchar(10),
+    password varchar(50)
+    ); 
+    create table properties(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    title varchar(100) not null,
+    description varchar(200) not null,
+    locationid int,
+    userid int,
+    area int not null,
+    price int not null,
+    FOREIGN KEY(locationid) REFERENCES locations(id),
+    FOREIGN KEY(userid) REFERENCES users(id)
+    );
