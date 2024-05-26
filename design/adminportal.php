70777c4736742b4588af55968390553920049f3b
Suresh-Tamang <sureshjimba3333@gmail.com>
2024-05-26 23:56:48 +0545

    managed

---
+++ design/adminportal.php
@@ -0,0 +1,87 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Propertylord</title>
+    <link rel="stylesheet" href="addproperty.css">
+    <!-- boxi icon links -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">    
+</head>
+<body>
+    <header class="sticky">
+        <a href="#">
+            <img src="img/logo.png" alt="">
+        </a>
+        <ul class="navbar open">
+            <li><a href="#">Home</a></li>
+            <li><a href="index.php#aboutus">About Us</a></li>
+            <li><a href="#">contact us</a></li>
+            <li><a href="#">Renting</a></li>
+            <li><a href="#">Selling</a></li>
+        </ul>
+        <div class="h-btn">
+            <!-- <a href="#" class="h-btn1 login">Cancel</a> -->
+            <!-- <a href="user.php" class="h-btn2">Cancel</a> -->
+            <div class="bx bx-menu" id="menu-icon"></div>
+        </div>
+    </header>
+
+    <!-- registration section -->
+<section>
+
+</section>
+<section>
+
+</section>
+    <!-- footer -->
+    <section class="footer">
+        <div class="footer-content">
+            <img src="img/logo.png" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, et.
+            </p>
+            <div class="icons">
+                <a href="#"><i class='bx bxl-facebook'></i></a>
+                <a href="#"><i class='bx bxl-instagram'></i></a>
+                <a href="#"><i class='bx bxl-twitter'></i></a>
+                <a href="#"><i class='bx bxl-linkedin'></i></a>
+            </div>
+        </div>
+        <div class="footer-content links">
+            <h4>Services</h4>
+            <li><a href="#">Houses</a></li>
+            <li><a href="#">Apartments</a></li>
+            <li><a href="#">Flats</a></li>
+            <li><a href="#">Renting</a></li>
+        </div>
+        <div class="footer-content links">
+            <h4>About us</h4>
+            <li><a href="#">Houses</a></li>
+            <li><a href="#">Apartments</a></li>
+            <li><a href="#">Flats</a></li>
+            <li><a href="#">Renting</a></li>
+        </div>
+        
+        <div class="footer-content links">
+            <h4>Popular Projects</h4>
+            <li><a href="#">Houses</a></li>
+            <li><a href="#">Apartments</a></li>
+            <li><a href="#">Flats</a></li>
+            <li><a href="#">Renting</a></li>
+        </div>
+    </section>
+    <section class="cp">
+        <div class="copyright">
+            <i class='bx bx-copyright'>PropertyLord All Right Reserved</i>
+        </div>
+    </section>s
+    
+
+    <!-- js file -->
+    <script src="script.js"></script>
+</body>
+
+</html>
